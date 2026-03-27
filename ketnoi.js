21c8c67ebc625e20b4903e445c8266506f64f041
phanmemquanli <phanmemgiaoduclc@gmail.com>
2026-03-28 02:07:32 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -1,3 +1,4 @@
+// --- CẤU HÌNH HỆ THỐNG ---
 const linkApi = "https://script.google.com/macros/s/AKfycbwLFjHvjGSLMFvdGLYaj6W4Cu-Da3KFsLHBOgezQKtPwSzBT65CIaJG9frfS-vM7PVsTg/exec"; 
 const hanDung = "31/12/2026"; 
 
@@ -13,6 +14,7 @@ async function goiMyChuHien(tenLenh, tLieu = {}) {
         });
         return await ph.json();
     } catch (loi) {
+        console.error("Lỗi Fetch API:", loi);
         throw new Error("Lỗi kết nối mạng. Vui lòng kiểm tra lại!");
     }
 }
